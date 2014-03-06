ec6e589021625043aa29dac2e63dbedd8932be5d
Nicolas Thomas <nicolas.thomas92@gmail.com>
2014-03-06 20:33:05 +0100

    vérif JS passwords
    Signed-off-by: Nicolas Thomas <nicolas.thomas92@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -12,4 +12,24 @@ $(document).ready(function(){
 	$(window).scroll(function() {
 		EasyPeasyParallax();
 	});
+	
+	$("#send").click(function(e){
+	e.preventDefault();
+	valid = true;
+	if($("#password").val() == "" || $("#password_verif").val() == ""){
+		$("#erreur").fadeIn();
+		valid = false;
+	}
+	
+	if($("#password").val() != $("#password_verif").val() ){
+		$("#different_passwords").fadeIn();
+		valid = false;
+		
+	}
+	
+	return valid;
 });
+	
+	
+});
+
